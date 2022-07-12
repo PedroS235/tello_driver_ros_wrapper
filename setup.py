da5b04157f440a3135c5dd07b863a4421daeaf1f
Pedro <pedros235@github.com>
2022-07-12 22:27:45 +0200

    Fixing package name

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup
 
-package_name = 'tello_ros'
+package_name = 'tello_ros_wrapper'
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        (
+            'share/ament_index/resource_index/packages',
+            ['resource/' + package_name],
+        ),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
@@ -19,7 +21,6 @@ setup(
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
-        ],
+        'console_scripts': [],
     },
 )
